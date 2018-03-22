942d16fa6e86eaf512f80c4d15674a8f64fc84e2
theminecoder <buisness@theminecoder.me>
2018-03-22 18:16:52 +1100

    Bump out mixin-related files to be restored on new branch

--- src/main/java/me/theminecoder/minecraft/nmsproxy/NMSProxyPlugin.java
+++ src/main/java/me/theminecoder/minecraft/nmsproxy/NMSProxyPlugin.java
@@ -17,46 +17,4 @@ public class NMSProxyPlugin extends JavaPlugin {
         NMS_VERSION = packageName.substring(packageName.lastIndexOf(".") + 1);
     }
 
-//    @Override
-//    public void onEnable() {
-//        SimpleMixinInjectionProvider mixinInjectionProvider = new SimpleMixinInjectionProvider();
-//        Arrays.stream(getServer().getPluginManager().getPlugins())
-//                .filter(plugin -> plugin instanceof PluginMixinProvider)
-//                .forEach(plugin -> ((PluginMixinProvider) plugin).provideMixins(mixinInjectionProvider));
-//
-//        ByteBuddy byteBuddy = new ByteBuddy();
-//        TypePool typePool = TypePool.Default.of(Bukkit.class.getClassLoader());
-//        Map<String, DynamicType.Builder> builders = new HashMap<>();
-//        mixinInjectionProvider.getMixinClasses().forEach(mixinClass -> {
-//            NMSClass classAnnotation = mixinClass.getAnnotation(NMSClass.class);
-//            if (classAnnotation == null) {
-//                System.out.println("[MIXIN] Skipping " + mixinClass.getCanonicalName() + " as it has no @NMSClass annotation!");
-//                return;
-//            }
-//
-//            String nmsClassName = classAnnotation.type().getClassName(NMS_VERSION, classAnnotation.className());
-//
-//            DynamicType.Builder builder = builders.get(nmsClassName);
-//            if (builder == null) {
-//                TypeDescription result = typePool.describe(nmsClassName).resolve();
-//                builder = byteBuddy.rebase(
-//                        result,
-//                        ClassFileLocator.ForClassLoader.of(Bukkit.class.getClassLoader())
-//                );
-//                builders.put(nmsClassName, builder);
-//            }
-//
-//            DynamicType.Builder classBuilder = builder;
-//            Arrays.stream(mixinClass.getDeclaredMethods()).forEach(mixinMethod -> {
-//                NMSMixin mixinAnnotation = mixinMethod.getAnnotation(NMSMixin.class);
-//                if (mixinAnnotation == null) {
-//                    return;
-//                }
-//
-//                classBuilder.method()
-//            });
-//        });
-//
-//        builders.forEach((clazz, builder) -> builder.make().load(Bukkit.class.getClassLoader()));
-//    }
 }
--- src/main/java/me/theminecoder/minecraft/nmsproxy/mixin/NMSMixinClass.java
+++
@@ -1,7 +0,0 @@
-package me.theminecoder.minecraft.nmsproxy.mixin;
-
-/**
- * @author theminecoder
- */
-public abstract class NMSMixinClass {
-}
